42a127a7f9e66c9dee80d2d95d6426762dc81b4d
littleOctopus75 <127283227+littleOctopus75@users.noreply.github.com>
2024-04-11 20:09:33 -0600

    ajuste de semaforo
    Las coordenadas del semaforo se pasaron en variables, para facilitar su ubicación

--- semaforo.js
+++ semaforo.js
@@ -14,24 +14,25 @@ let semaforo;
 let tiempoUltimoCambio = 0;
 
 function setup() {
-  createCanvas(200, 400);
-  semaforo = new Semaforo(2000, 1000); // Ejemplo: verde 2 segundos, rojo 1 segundo
+  createCanvas(400, 600);
+  semaforo = new Semaforo(1000, 500); // Ejemplo: verde 2 segundos, rojo 1 segundo
 }
 
 function draw() {
+  var posicionesX=190;
+  var posicionesY=5;
   background(255);
-
   // Dibuja el semáforo
   stroke(0);
   fill(255);
-  rect(75, 50, 50, 150);
-  line(100, 200, 100, 300);
+  rect(posicionesX, posicionesY, 50, 150);
+  line(posicionesX+25, posicionesY+150, posicionesX+25, 300);
 
   fill(semaforo.estado === 'verde' ?  color(0, 255, 0):color(255, 255, 255) );
-  ellipse(100, 90, 50, 50);
+  ellipse(posicionesX+25, posicionesY+30, 50, 50);
 
   fill(semaforo.estado === 'rojo' ? color(255, 0, 0) : color(255,255,255));
-  ellipse(100, 160, 50, 50);
+  ellipse(posicionesX+25, posicionesY+110, 50, 50);
 
   // Cambia el color del semáforo si ha pasado el tiempo
   if (millis() - tiempoUltimoCambio >= (semaforo.estado === 'rojo' ? semaforo.timerRojo : semaforo.timerVerde)) {
