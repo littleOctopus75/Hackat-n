cbdd368abac3f436afb15268ca60ec07a8e63f12
littleOctopus75 <127283227+littleOctopus75@users.noreply.github.com>
2024-04-12 14:53:02 -0600

    Revert "Revert "Merge branch 'main' into Luis""
    This reverts commit e6b83b550a53a4207ee4f6eca7a6006714228ef6.

--- cuadroConfiguracion.js
+++ cuadroConfiguracion.js
@@ -1,35 +1,44 @@
 class CuadroConfiguracion {
   constructor(semaforo, carretera) {
-    this.semaforo=semaforo;
-    this.carretera=carretera;
+    this.semaforo = semaforo;
+    this.carretera = carretera;
 
     // Crear las cajas de texto para la frecuencia de aparición
     this.frecuenciaInput = createInput();
-    this.frecuenciaInput.position(1000, 260);
+    this.frecuenciaInput.position(1020, 260);
 
     // Crear las cajas de texto para el tiempo del semáforo
     this.tiempoVerdeInput = createInput();
-    this.tiempoVerdeInput.position(1000, 210);
+    this.tiempoVerdeInput.position(1020, 210);
     this.tiempoRojoInput = createInput();
     this.tiempoRojoInput.position(1300, 210);
 
     this.boton = createButton("Actualizar");
-    this.boton.position(1350, 260);
+    this.boton.style('width', '150px'); 
+    this.boton.style('height', '30px'); 
+    this.boton.position(1310, 260);
     this.boton.mousePressed(() => this.actualizarConfiguracion()); // Asociar función al botón
-}
+  }
 
-actualizarConfiguracion() {
-  // Obtener valores de las cajas de texto
-  //this.frecuenciaInput.value();
-  this.semaforo.setTiempoVerde( this.tiempoVerdeInput.value());
-  this.semaforo.setTiempoRojo(this.tiempoRojoInput.value());
-  this.carretera.setIntervalosCarro(this.frecuenciaInput.value());
+  actualizarConfiguracion() {
+    // Obtener valores de las cajas de texto
+    let tiempoVerdeSegundos = this.tiempoVerdeInput.value() !== '' ? parseFloat(this.tiempoVerdeInput.value()) * 1000 : 1000;
+    let tiempoRojoSegundos = this.tiempoRojoInput.value() !== '' ? parseFloat(this.tiempoRojoInput.value()) * 1000 : 1000;
+    let frecuenciaSegundos = this.frecuenciaInput.value() !== '' ? parseFloat(this.frecuenciaInput.value()) * 1000 : 1000;
 
-}
+    this.semaforo.setTiempoVerde(tiempoVerdeSegundos);
+    this.semaforo.setTiempoRojo(tiempoRojoSegundos);
+    this.carretera.setIntervalosCarro(frecuenciaSegundos);
+  }
+
+
   mostrarConfiguraciones() {
-    text("Configuraciones semaforo: ", 780, 35);
-    text("Tiempo Verde", 1015, 35);
+    text("Configuraciones semáforo: ", 780, 62);
+    text("S", 1200, 62);
+    text("S", 1480, 62);
+
+    text("Tiempo Verde", 1120, 35);
     text("Tiempo Rojo", 1315, 35);
-    text("Tiempo de aparicion vehiculos: ", 750, 110);
+    text("Tiempo de aparición vehículos: ", 780, 110);
   }
 }
